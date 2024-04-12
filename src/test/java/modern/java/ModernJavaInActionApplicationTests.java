5ddcf0e0fae33eef5413d7ce697e4dbba80838bc
leejinwoo <leejinwoo1126@gmail.com>
2024-04-12 20:13:45 +0900

    init

---
+++ src/test/java/modern/java/ModernJavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package modern.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
